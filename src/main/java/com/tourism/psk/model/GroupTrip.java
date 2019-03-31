be7285d1c3d9744f16123947462c89d72228a9da
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2019-03-31 21:05:53 +0300

    adding group trip

---
+++ src/main/java/com/tourism/psk/model/GroupTrip.java
@@ -0,0 +1,63 @@
+package com.tourism.psk.model;
+
+import com.tourism.psk.model.response.Trip;
+
+import javax.persistence.Entity;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class GroupTrip {
+
+    private String name;
+    private String description;
+    private Set<Trip> trips;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Trip> getTrips() {
+        return trips;
+    }
+
+    public void setTrips(Set<Trip> trips) {
+        this.trips = trips;
+    }
+
+    @Override
+    public String toString() {
+        return "GroupTrip{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", trips=" + trips +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupTrip groupTrip = (GroupTrip) o;
+        return Objects.equals(name, groupTrip.name) &&
+                Objects.equals(description, groupTrip.description) &&
+                Objects.equals(trips, groupTrip.trips);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, trips);
+    }
+}
