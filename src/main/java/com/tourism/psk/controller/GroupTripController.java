0c255986b3734e2537acfda6e3899b406c0678dd
Mindaugas <mindaugas.barcauskas@mif.stud.vu.lt>
2019-04-10 21:48:47 +0300

    refactoring controllers

---
+++ src/main/java/com/tourism/psk/controller/GroupTripController.java
@@ -0,0 +1,55 @@
+package com.tourism.psk.controller;
+
+import com.tourism.psk.model.GroupTrip;
+import com.tourism.psk.model.request.GroupTripRequest;
+import com.tourism.psk.service.SessionService;
+import com.tourism.psk.service.TripService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class GroupTripController {
+
+    private final TripService tripService;
+    private final SessionService sessionService;
+
+    @Autowired
+    public GroupTripController(TripService tripService, SessionService sessionService) {
+        this.tripService = tripService;
+        this.sessionService = sessionService;
+    }
+
+    @RequestMapping(value = "/groupTrip", method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addGroupTrip(@RequestHeader("Authorization") String authToken, @RequestBody final GroupTrip groupTrip) {
+        sessionService.authenticate(authToken);
+        tripService.addGroupTrip(groupTrip);
+    }
+
+    @RequestMapping(value = "/addGroupTrip", method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void addGroupTripRequest(@RequestHeader("Authorization") String authToken,
+                                    @RequestBody final GroupTripRequest groupTripRequest) {
+        sessionService.authenticate(authToken);
+        tripService.addGroupTripThroughRequest(groupTripRequest);
+    }
+
+    @RequestMapping(value = "/groupTrips", method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<GroupTrip> findGroupTrips(@RequestHeader("Authorization") String authToken) {
+        sessionService.authenticate(authToken);
+        return tripService.findGroupTrips();
+    }
+
+    @RequestMapping(value = "/groupTrip", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public GroupTrip findGroupTrip(@RequestHeader("Authorization") String authToken,@RequestParam("id") long groupTripId) {
+        sessionService.authenticate(authToken);
+        return tripService.findGroupTrip(groupTripId);
+    }
+
+
+}
--- src/main/java/com/tourism/psk/controller/TripController.java
+++ src/main/java/com/tourism/psk/controller/TripController.java
@@ -25,34 +25,6 @@ public class TripController {
     }
 
 
-    @RequestMapping(value = "/groupTrip", method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE,
-            consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addGroupTrip(@RequestHeader("Authorization") String authToken, @RequestBody final GroupTrip groupTrip) {
-        sessionService.authenticate(authToken);
-        tripService.addGroupTrip(groupTrip);
-    }
-
-    @RequestMapping(value = "/addGroupTrip", method = RequestMethod.POST,
-                consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void addGroupTripRequest(@RequestHeader("Authorization") String authToken,
-                                    @RequestBody final GroupTripRequest groupTripRequest) {
-        sessionService.authenticate(authToken);
-        tripService.addGroupTripThroughRequest(groupTripRequest);
-    }
-
-    @RequestMapping(value = "/groupTrips", method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public List<GroupTrip> findGroupTrips(@RequestHeader("Authorization") String authToken) {
-        sessionService.authenticate(authToken);
-        return tripService.findGroupTrips();
-    }
-
-    @RequestMapping(value = "/groupTrip", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public GroupTrip findGroupTrip(@RequestHeader("Authorization") String authToken,@RequestParam("id") long groupTripId) {
-        sessionService.authenticate(authToken);
-        return tripService.findGroupTrip(groupTripId);
-    }
     @RequestMapping(value = "/find/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Trip> findAllTrips() {
         return tripService.findAll();
