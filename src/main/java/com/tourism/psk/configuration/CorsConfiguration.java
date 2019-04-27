03a2af860298502afca25c2497f2893809ace265
Tautvydas Stukenas <tautvydas.stukenas@gmail.com>
2019-04-27 17:46:12 +0300

    Potential CORS fix v5

--- src/main/java/com/tourism/psk/configuration/CorsConfiguration.java
+++ src/main/java/com/tourism/psk/configuration/CorsConfiguration.java
@@ -1,33 +1,18 @@
 package com.tourism.psk.configuration;
 
 
-import com.tourism.psk.constants.Globals;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class CorsConfiguration {
+@EnableWebMvc
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-                /*
-                        .allowedOrigins("*")
-                        .allowedMethods("POST", "PUT", "GET", "DELETE", "PATCH")
-                        .allowedHeaders(Globals.ACCESS_TOKEN_HEADER_NAME, "Content-Type")
-                        .exposedHeaders(Globals.ACCESS_TOKEN_HEADER_NAME)
-                        .allowCredentials(true)
-                        .maxAge(3600);
-
-                 */
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
     }
 }
