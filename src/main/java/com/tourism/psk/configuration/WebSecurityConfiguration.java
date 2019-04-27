277f77fe898db34e9d7276c3e6ca192e3cea968c
Tautvydas Stukenas <tautvydas.stukenas@gmail.com>
2019-04-27 17:54:15 +0300

    Potential CORS fix v6

---
+++ src/main/java/com/tourism/psk/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.tourism.psk.configuration;
+
+import com.tourism.psk.constants.Globals;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Value("${cors.origins}")
+    private List<String> allowedOrigins;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();//.and().csrf().disable().authorizeRequests().anyRequest().permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().anyRequest();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource()
+    {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(allowedOrigins);
+        configuration.setAllowedMethods(Globals.CORS_ALLOWED_METHODS);
+        configuration.setAllowedHeaders(Globals.CORS_ALLOWED_HEADERS);
+        configuration.setExposedHeaders(Globals.CORS_ALLOWED_HEADERS);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
