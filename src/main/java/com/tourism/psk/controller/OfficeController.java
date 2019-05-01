665e8a432c34a2354dc542722195f030303c4165
Rytis Bankieta <rytis.bankieta@gmail.com>
2019-05-01 13:37:13 +0300

    add missing postgresql driver

--- src/main/java/com/tourism/psk/controller/OfficeController.java
+++ src/main/java/com/tourism/psk/controller/OfficeController.java
@@ -2,14 +2,11 @@ package com.tourism.psk.controller;
 
 import com.tourism.psk.constants.Globals;
 import com.tourism.psk.model.Office;
-import com.tourism.psk.model.Session;
 import com.tourism.psk.service.OfficeService;
 import com.tourism.psk.service.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
