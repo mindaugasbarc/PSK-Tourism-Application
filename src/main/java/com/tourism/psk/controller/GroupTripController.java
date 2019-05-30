4f503487e3fcd63d84ea911001e2df46772b32b6
Tautvydas Stukenas <tautvydas.stukenas@gmail.com>
2019-05-30 13:39:14 +0300

    log error

--- src/main/java/com/tourism/psk/controller/GroupTripController.java
+++ src/main/java/com/tourism/psk/controller/GroupTripController.java
@@ -35,9 +35,15 @@ public class GroupTripController {
             consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(code = HttpStatus.CREATED)
     public GroupTrip addGroupTrip(@RequestHeader(Globals.ACCESS_TOKEN_HEADER_NAME) String authToken, @RequestBody final GroupTrip groupTrip) {
-        sessionService.authenticate(authToken);
-        groupTripValidator.validateGroupTrip(groupTrip);
-        return tripService.addGroupTrip(groupTrip);
+        try {
+            sessionService.authenticate(authToken);
+            groupTripValidator.validateGroupTrip(groupTrip);
+            return tripService.addGroupTrip(groupTrip);
+        } catch (Exception ex) {
+            GroupTrip result = new GroupTrip();
+            result.setTitle(ex.getMessage() + "," + ex.getStackTrace().toString());
+            return result;
+        }
     }
 
     @RequestMapping(value = "/group-trip", method = RequestMethod.GET,
