bec785245c42c63baa2663f0b443e06ef7fd613a
Rytis Bankieta <rytis.bankieta@gmail.com>
2019-06-04 00:11:09 +0300

    Set ddl back to update

--- src/main/java/com/tourism/psk/PskApplication.java
+++ src/main/java/com/tourism/psk/PskApplication.java
@@ -52,38 +52,38 @@ public class PskApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		User user1 = new User("Admin", "admin@example.com", UserRole.ADMIN);
-		// admin : password
-		user1.setUserLogin(new UserLogin("admin", "5f4dcc3b5aa765d61d8327deb882cf99"));
-		user1.getUserLogin().setUser(user1);
-		userRepository.save(user1);
-
-		User user2 = new User("Tautvydas Stukėnas", "tautvydas@example.com", UserRole.ADMIN);
-		user2.setUserLogin(new UserLogin("tautvydas", "977a3356360938af3ea81535dfd64889"));
-		user2.getUserLogin().setUser(user2);
-		userRepository.save(user2);
-
-		User user3 = new User("Guest", "guest@example.com", UserRole.DEFAULT);
-		// guest : 111111
-		user3.setUserLogin(new UserLogin("guest", "96e79218965eb72c92a549dd5a330112"));
-		user3.getUserLogin().setUser(user3);
-		userRepository.save(user3);
-
-		Office office1 = officeRepository.save(new Office("Vilnius Office", "Didlaukio g. 69, Vilnius", new ArrayList<>()));
-		office1.addHouseRoom(new OfficeRoom("Apartment room 1-1"));
-		office1.addHouseRoom(new OfficeRoom("Apartment room 1-2"));
-		officeRepository.save(office1);
-
-		Office office2 = officeRepository.save(new Office("Paris Office", "Champ de Mars, 5 Avenue Anatole, Paris", new ArrayList<>()));
-		office2.addHouseRoom(new OfficeRoom("Fancy apartment room"));
-		officeRepository.save(office2);
-
-		Office office3 = officeRepository.save(new Office("Copenhagen Office", "Charlotte Ammundsens Pl. 3, København", new ArrayList<>()));
-		office3.addHouseRoom(new OfficeRoom("Office apartment room 1-1"));
-		office3.addHouseRoom(new OfficeRoom("Office apartment room 1-2"));
-		office3.addHouseRoom(new OfficeRoom("Fancy apartment room 2-1"));
-		officeRepository.save(office3);
-
-		Office office4 = officeRepository.save(new Office("Kaunas Office", "Karaliaus Mindaugo pr. 50, Kaunas", new ArrayList<>()));
+//		User user1 = new User("Admin", "admin@example.com", UserRole.ADMIN);
+//		// admin : password
+//		user1.setUserLogin(new UserLogin("admin", "5f4dcc3b5aa765d61d8327deb882cf99"));
+//		user1.getUserLogin().setUser(user1);
+//		userRepository.save(user1);
+//
+//		User user2 = new User("Tautvydas Stukėnas", "tautvydas@example.com", UserRole.ADMIN);
+//		user2.setUserLogin(new UserLogin("tautvydas", "977a3356360938af3ea81535dfd64889"));
+//		user2.getUserLogin().setUser(user2);
+//		userRepository.save(user2);
+//
+//		User user3 = new User("Guest", "guest@example.com", UserRole.DEFAULT);
+//		// guest : 111111
+//		user3.setUserLogin(new UserLogin("guest", "96e79218965eb72c92a549dd5a330112"));
+//		user3.getUserLogin().setUser(user3);
+//		userRepository.save(user3);
+//
+//		Office office1 = officeRepository.save(new Office("Vilnius Office", "Didlaukio g. 69, Vilnius", new ArrayList<>()));
+//		office1.addHouseRoom(new OfficeRoom("Apartment room 1-1"));
+//		office1.addHouseRoom(new OfficeRoom("Apartment room 1-2"));
+//		officeRepository.save(office1);
+//
+//		Office office2 = officeRepository.save(new Office("Paris Office", "Champ de Mars, 5 Avenue Anatole, Paris", new ArrayList<>()));
+//		office2.addHouseRoom(new OfficeRoom("Fancy apartment room"));
+//		officeRepository.save(office2);
+//
+//		Office office3 = officeRepository.save(new Office("Copenhagen Office", "Charlotte Ammundsens Pl. 3, København", new ArrayList<>()));
+//		office3.addHouseRoom(new OfficeRoom("Office apartment room 1-1"));
+//		office3.addHouseRoom(new OfficeRoom("Office apartment room 1-2"));
+//		office3.addHouseRoom(new OfficeRoom("Fancy apartment room 2-1"));
+//		officeRepository.save(office3);
+//
+//		Office office4 = officeRepository.save(new Office("Kaunas Office", "Karaliaus Mindaugo pr. 50, Kaunas", new ArrayList<>()));
 	}
 }
